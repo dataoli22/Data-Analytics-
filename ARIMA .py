ee71c948893e1c9b4043a4cbab2343189b802fdd
Olipriya Chakraborty <117657044+dataoli22@users.noreply.github.com>
2023-10-21 21:56:06 +0530

    Add files via upload

---
+++ ARIMA .py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import  yfinance as yf
+
+gs= yf.download('ril', start="2011-1-1", end= "2023-03-01")
+
+
+# In[20]:
+
+
+import pandas as pd
+
+dataset= gs.copy()
+dataset= dataset.reset_index()
+dataset['Date']=pd.to_datetime(dataset['Date'])
+dataset.set_index('Date', inplace=True)
+dataset= dataset['Close'].to_frame()
+
+
+# In[21]:
+
+
+from pmdarima.arima import auto_arima
+#Auto arima to select optional ARIMA parameters
+model= auto_arima(dataset['Close'], seasonal= False, trace= True)
+print(model.summary())
+
+
+# In[24]:
+
+
+import statsmodels.api as smapi
+import numpy as np
+
+def arima_forecast(history):
+    model = smapi.tsa.arima.ARIMA(history, order=(0,1,0))
+    model_fit = model.fit()
+    
+    output= model_fit.forecast()
+    yhat= output[0]
+    return yhat
+
+X= dataset.values
+size= int(len(X)* 0.8)
+train, test= X[0:size], X[size:len(X)]
+
+history= [x for x in train]
+predictions= list()
+for t in range(len(test)):
+    yhat= arima_forecast(history)
+    predictions.append(yhat)
+    obs= test[t]
+    history.append(obs)
+
+
+# In[25]:
+
+
+
+import matplotlib.pyplot as plt
+
+
+plt.figure(figsize=(12, 6), dpi=100)
+plt.plot(dataset.iloc[size:,:], test, label='Real')
+plt.plot(dataset.iloc[size:,:], predictions, color='red', label='Predicted')
+plt.title('ARIMA Predictions vs Actual Values')
+plt.xlabel('Date')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
+
+
+# In[26]:
+
+
+# Calculate the Fourier Transform
+data_FT = dataset[['Close']]
+close_fft = np.fft.fft(np.asarray(data_FT['Close'].tolist()))
+fft_df = pd.DataFrame({'fft':close_fft})
+fft_df['absolute'] = fft_df['fft'].apply(lambda x: np.abs(x))
+fft_df['angle'] = fft_df['fft'].apply(lambda x: np.angle(x))
+
+# Plot the Fourier Transforms
+
+plt.figure(figsize=(14, 7), dpi=100)
+plt.plot(np.asarray(data_FT['Close'].tolist()),  label='Real')
+for num_ in [3, 6, 9]:
+    fft_list_m10= np.copy(close_fft); fft_list_m10[num_:-num_]=0
+    plt.plot(np.fft.ifft(fft_list_m10), label='Fourier transform with {} components'.format(num_))
+plt.xlabel('Days')
+plt.ylabel('USD')
+plt.title('Goldman Sachs (close) stock prices & Fourier transforms')
+plt.legend()
+plt.show()
+
+
+# In[27]:
+
+
+# Calculate EMA
+def ema(close, period=20):
+    return dataset.ewm(span=period, adjust=False).mean()
+
+
+def rsi(close, period=14):
+    delta= close.diff()
+    gain, loss= delta.copy(), delta.copy()
+    gain[gain<0]=0
+    loss[loss>0]=0
+    avg_gain= gain.rolling(period).mean()
+    avg_loss= abs(loss.rolling(period).mean())
+    rs= avg_gain/avg_loss
+    rsi= 100.0- (100.0/(1.0+rs))
+    return rsi
+    
+
+
+# In[28]:
+
+
+# Calculate MACD
+def macd(close, fast_period=12, slow_period=26, signal_period=9):
+    fast_ema = close.ewm(span=fast_period, adjust=False).mean()
+    slow_ema = close.ewm(span=slow_period, adjust=False).mean()
+    macd_line = fast_ema - slow_ema
+    signal_line = macd_line.ewm(span=signal_period, adjust=False).mean()
+    histogram = macd_line - signal_line
+    return macd_line
+
+# Calculate OBV
+def obv(close, volume):
+    obv = np.where(close > close.shift(), volume, np.where(close < close.shift(), -volume, 0)).cumsum()
+    return obv
+
+
+# In[29]:
+
+
+# Add technical indicators to dataset DF
+dataset['ema_20'] = ema(["Close"], 20)
+dataset['ema_50'] = ema(["Close"], 50)
+dataset['ema_100'] = ema(["Close"], 100)
+
+dataset['rsi'] = rsi(["Close"])
+dataset['macd'] = macd(["Close"])
+dataset['obv'] = obv(["Close"],["Volume"])
+
+
+# In[30]:
+
+
+# Create arima DF using predictions
+arima_df = pd.DataFrame(index=dataset.index, columns=['ARIMA'])
+
+# Set Fourier Transforms DF
+'''
+fft_df.reset_index(inplace=True)
+fft_df['index'] = pd.to_datetime(dataset.index)
+fft_df.set_index('index', inplace=True)
+'''
+
+fft_df_real = pd.DataFrame(np.real(fft_df['fft']), index=fft_df.index, columns=['Fourier_real'])
+fft_df_imag = pd.DataFrame(np.imag(fft_df['fft']), index=fft_df.index, columns=['Fourier_imag'])
+
+# Technical Indicators DF
+technical_indicators_df = dataset[['ema_20', 'ema_50', 'ema_100', 'rsi', 'macd', 'Close']]
+
+# Merge DF
+merged_df = pd.concat([arima_df, fft_df_real, fft_df_imag, technical_indicators_df], axis=1)
+merged_df = merged_df.dropna()
+merged_df
+
+
+# In[31]:
+
+
+# Separate in Train and Test Dfs
+train_size = int(len(merged_df) * 0.8)
+train_df, test_df = merged_df.iloc[:train_size], merged_df.iloc[train_size:]
+
+
+# In[14]:
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+# Scale the features
+scaler = MinMaxScaler()
+train_scaled = scaler.fit_transform(train_df.drop('Close', axis=1))
+test_scaled = scaler.transform(test_df.drop('Close', axis=1))
+
+# Convert the scaled data back to a DataFrame
+train_scaled_df = pd.DataFrame(train_scaled, columns=train_df.columns[:-1], index=train_df.index)
+test_scaled_df = pd.DataFrame(test_scaled, columns=test_df.columns[:-1], index=test_df.index)
+
+# Merge the scaled features with the target variable
+train_scaled_df['Close'] = train_df['Close']
+test_scaled_df['Close'] = test_df['Close']
+
+# Split the scaled data into Features and Label
+X_train = train_scaled_df.iloc[:, :-1].values
+y_train = train_scaled_df.iloc[:, -1].values
+X_test = test_scaled_df.iloc[:, :-1].values
+y_test = test_scaled_df.iloc[:, -1].values
+
+
+# In[15]:
+
+
+# Import keras modules
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping
+
+# Define model
+model = Sequential()
+model.add(Dense(32, activation='relu', input_dim=X_train.shape[1]))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1, activation='linear'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mse')
+
+
+# Define the early stopping callback
+early_stop = EarlyStopping(monitor='val_loss', patience=20, verbose=1, mode='min')
+
+# Train the model with early stopping callback
+history = model.fit(X_train, y_train, epochs=1000, batch_size=32, verbose=1, validation_data=(X_test, y_test), callbacks=[early_stop], shuffle=False)
+
+
+# In[ ]:
+
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, explained_variance_score
+
+import numpy as np
+y_pred = model.predict(X_test)
+
+# Calculate test metrics
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+mae = mean_absolute_error(y_test, y_pred)
+
+r2 = r2_score(y_test, y_pred)
+evs = explained_variance_score(y_test, y_pred)
+
+mape = np.mean(np.abs((y_test - y_pred) / y_test)) * 100
+mpe = np.mean((y_test - y_pred) / y_test) * 100
+
+print(f"Mean Squared Error (MSE): {mse}")
+print(f"Mean Absolute Error (MAE): {mae}")
+print(f"R2 Score: {r2}")
+print(f"Explained Variance Score: {evs}")
+print(f"Mean Absolute Percentage Error (MAPE): {mape}")
+print(f"Mean Percentage Error (MPE): {mpe}")
+
+
+# In[16]:
+
+
+# Plot final Predictions
+plt.figure(figsize=(12, 6), dpi=100)
+plt.plot(test_scaled_df.index, y_test, label='Real')
+plt.plot(test_scaled_df.index, y_pred, color='red', label='Predicted')
+plt.title('Model Predictions vs Real Prices')
+plt.xlabel('Date')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
